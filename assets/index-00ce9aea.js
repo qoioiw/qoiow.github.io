985ade24d0ac7291f15d19d894eacf73f8ab469c
白帝子 <address>
2023-12-23 21:02:36 +0800

    修改url

--- assets/index-00ce9aea.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,f as n}from"./index-f7d132ed.js";const o={};function r(t,s){return c(),n("div",null,"404")}const a=e(o,[["render",r]]);export{a as default};
--- assets/setting-7c65be5e.js
+++
@@ -1,1 +0,0 @@
-import{P as e}from"./index-f7d132ed.js";const r=e("SettingStore",{state:()=>({fold:!1,refsh:!1})});export{r as u};
