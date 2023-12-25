7e5e2e02a2d64061541ba00532a307f3ae2d6050
白帝子 <address>
2023-12-25 10:38:49 +0800

    去掉括号

--- assets/setting-dbebbd77.js
+++
@@ -1,1 +0,0 @@
-import{P as e}from"./index-f77b7d88.js";const r=e("SettingStore",{state:()=>({fold:!1,refsh:!1})});export{r as u};
---
+++ assets/setting-f005889d.js
@@ -0,0 +1,1 @@
+import{P as e}from"./index-a1c0bef5.js";const r=e("SettingStore",{state:()=>({fold:!1,refsh:!1})});export{r as u};
